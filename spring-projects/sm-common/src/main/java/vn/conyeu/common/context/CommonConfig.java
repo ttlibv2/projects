78237c0142b25261378195db4a12e4135ac60ab2
NGUYEN QUOC TUAN <tuanit.nguyenquoc@gmail.com>
2024-06-16 07:04:48 +0700

    n

--- spring-projects/sm-common/src/main/java/vn/conyeu/common/context/CommonConfig.java
+++ spring-projects/sm-common/src/main/java/vn/conyeu/common/context/CommonConfig.java
@@ -5,9 +5,15 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import vn.conyeu.common.domain.I18N;
 import vn.conyeu.commons.utils.MapperHelper;
+
+import java.util.List;
+import java.util.Locale;
 
 @Configuration
 public class CommonConfig {
@@ -17,11 +23,27 @@ public class CommonConfig {
         return MapperHelper.mapper();
     }
 
+//    @Bean
+//    public MessageSource messageSource() {
+//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//        messageSource.setBasenames("messages");
+//        messageSource.setDefaultEncoding("UTF-8");
+//        return messageSource;
+//    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localResolver = new SessionLocaleResolver();
+        localResolver.setDefaultLocale(Locale.of("vi", "VN"));
+        return localResolver;
+    }
+
     @Bean
     public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasenames("messages");
-        messageSource.setDefaultEncoding("UTF-8");
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setCacheSeconds(10); //reload messages every 10 seconds
+        messageSource.setFileExtensions(List.of(".properties", ".xml", ".json"));
         return messageSource;
     }
 
