2b6ce7ce9079d1011fbd713999e0c1a0c6ab47b9
NGUYEN QUOC TUAN <tuanit.nguyenquoc@gmail.com>
2024-08-30 17:11:38 +0700

    0

--- spring-projects/sm-sheetdb/src/main/java/vn/conyeu/google/demo/SearchFile.java
+++ spring-projects/sm-sheetdb/src/main/java/vn/conyeu/google/demo/SearchFile.java
@@ -2,6 +2,7 @@ package vn.conyeu.google.demo;
 
 import com.querydsl.core.types.Template;
 import com.querydsl.core.types.Templates;
+import org.springframework.data.jpa.repository.support.Querydsl;
 import vn.conyeu.google.drives.GMime;
 import vn.conyeu.google.drives.builder.DriveQuery;
 import vn.conyeu.google.query.BoolExpression;
@@ -11,7 +12,8 @@ public class SearchFile {
 
     public static void main(String[] args) {
         Function<DriveQuery, BoolExpression> consumer = q -> q.name.eq("1222").and(q.mimeType.eq(GMime.AUDIO));
-        Templates
+        Templates;
+        com.querydsl.jpa.JPQLSerializer
     }
 
 
