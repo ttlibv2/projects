3273fe6cdc6851f9c3d10810f7046fd2c82af29d
NGUYEN QUOC TUAN <tuanit.nguyenquoc@gmail.com>
2025-03-11 14:36:15 +0700

    0

--- bk/tscli/packages/tscli/src/utilities/constant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_SCHEMATICS_COLLECTION = exports.globalFileName = exports.monorepoCfgFileName = void 0;
-exports.monorepoCfgFileName = ['monorepo.json'];
-exports.globalFileName = '.monorepo.json';
-exports.DEFAULT_SCHEMATICS_COLLECTION = '@schematics/angular';
-//# sourceMappingURL=constant.js.map
